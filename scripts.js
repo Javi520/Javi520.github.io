31d2e27702b3f4576454cc276747c0a6babf0235
Javi520 <javi520@live.com>
2019-08-25 21:42:24 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,5 +10,5 @@ window.onload=function fun_idade ()
 }
 window.onload = function menu_display()
 {
-    document.getElementById("menu").insertAdjacentHTML("beforebegin","<a href=""/"">Inicio</a>");
+    document.getElementById("menu").innerHTML("<a href=""/"">Inicio</a>");
 }
