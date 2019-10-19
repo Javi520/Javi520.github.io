df370121361ebe9f3b0cb4b0428463289c3ba821
Javi520 <javi520@live.com>
2019-10-19 23:56:01 +0200

    now more normal

--- scripts.js
+++ scripts.js
@@ -13,22 +13,11 @@ window.onload = function menu_display()
     document.getElementById("menu").innerHTML='<a href="/">Inicio</a>';
 }
 
-function aVer()
-{
-    if (Math.random()>.5)
-    {
-        sendMail();
-        alert("Ya te hablo yo");
-    }
-    else
-        alert("Hablame por instagram, te toco a ti");
-}
-
 function sendMail() {
     var link = "mailto:nashaun.kmauri@thtt.us"
              + "?cc=bedehiyen@ezmail.top"
-             + "&subject=" + escape("This is my subject")
-             + "&body=" + "tienes que hablar tu"
+             + "&subject=" + escape("Hola Javi")
+             + "&body=" + "Javi, tienes que hablar tu"
     ;
 
     window.location.href = link;
